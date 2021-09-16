c87bb2d134bfde1785394a8409bfcc3fc4ee0fa1
marghemolaro <89773853+marghemolaro@users.noreply.github.com>
2021-09-16 09:16:42 +0100

    Update getR.py

--- getR.py
+++ getR.py
@@ -9,7 +9,7 @@ from scipy import interpolate
 #zR_range.dat: zR models considered;  
 #R_correction.dat: R_zR(z_dash,k) values, with each column referring to values for a different zR model;
 
-#If you use this script for your scientific work, we kindly ask you to please reference: Molaro et al. in prep
+#If you use this script for your scientific work, we kindly ask you to please reference: Molaro et al. in prep (https://arxiv.org/abs/2109.06897) 
 
 #User-defined parameters
 z_dash 	= 5 #Observational redshift bin
